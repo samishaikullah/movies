7770ab05d6a9ab13f7298b228654a29b12b31e06
Shaik Sami Ullah <samishaikullah@gmail.com>
2026-02-08 17:19:35 -0600

    version5

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import MovieCard from "../components/MovieCard"
+import { useState } from "react"
+
+function Home() {
+
+    const [searchQuery, setSearchQuery] = useState ("");
+
+    const movies = [
+        {id: 1, title: "Version5", relase_date: "2020"},
+        {id: 2, title: "Terminator", relase_date: "1999"},
+        {id: 1, title: "The Matrix", relase_date: "1998"},
+        ]
+
+    const handleSearch = () => {
+
+    }    
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="searc-form">
+            <input type="text"
+             placeholder="Search for movies..."
+             className="search-input"
+             value={searchQuery}
+             onChange={(e) => setSearchQuery(e.target.value)}
+             />
+             <button type="submit" className="search-button">Search</button>
+        </form>
+        <div className="movies-grid">
+            {movies.map((movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && ( 
+                <MovieCard movie={movie} key={movie.id}/>
+            )
+            )}
+
+        </div>
+    </div>
+}
+
+export default Home
