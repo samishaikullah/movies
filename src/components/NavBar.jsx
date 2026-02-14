ac53c414e46fc97174586f8018782ca1d44ac2cf
Shaik Sami Ullah <samishaikullah@gmail.com>
2026-02-13 21:26:03 -0600

    Sami Streamss

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
 
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Sami Streams</Link>
+            <Link to="/">Sami Streamss</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-links">Home</Link>
